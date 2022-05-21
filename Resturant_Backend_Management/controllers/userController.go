c20807ec712bb9a5b3ae9ccb63daef09a006b344
Jacob <Jacobp1997@live.co.uk>
2022-05-21 20:41:26 +0100

    Controller updates

---
+++ Resturant_Backend_Management/controllers/userController.go
@@ -0,0 +1,35 @@
+package controller
+
+import "github.com/gin-gonic/gin"
+
+func GetUsers() gin.HandlerFunc {
+	return func(c *gin.Context) {
+
+	}
+}
+
+func GetUser() gin.HandlerFunc {
+	return func(c *gin.Context) {
+
+	}
+}
+
+func Signup() gin.HandlerFunc {
+	return func(c *gin.Context) {
+
+	}
+}
+
+func Login() gin.HandlerFunc {
+	return func(c *gin.Context) {
+
+	}
+}
+
+func HashPassword(password string) string {
+
+}
+
+func VerifyPassworf(userPassword string, providePassword string) (bool, string) {
+
+}
