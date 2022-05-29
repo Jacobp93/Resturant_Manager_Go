0bc716d71ea8a67b06d0b8af8745927b3e9ce7d6
Jacob <Jacobp1997@live.co.uk>
2022-05-29 13:11:31 +0100

    completed models

--- Resturant_Backend_Management/models/invoiceModel.go
+++ Resturant_Backend_Management/models/invoiceModel.go
@@ -8,11 +8,11 @@ import (
 
 type Invoice struct {
 	ID               primitive.ObjectID `bson:"_id"`
-	Invoice_id       string             `json"invoice_id"`
-	Order_id         string             `json"order_id"`
+	Invoice_id       string             `json:"invoice_id"`
+	Order_id         string             `json:"order_id"`
 	Payment_method   *string            `json:"payment_method" validate:"eq=CARD|eq=CASH|eq"`
 	Payment_status   *string            `json:"payment_status" validate:"required, eq=PENDING |eq=PAID|eq"`
-	Payment_due_date time.Time          `json: "Payment_due_date"`
+	Payment_due_date time.Time          `json:"Payment_due_date"`
 	Created_at       time.Time          `json:"created_at"`
-	updated_at       time.Time          `json:"updated_at"`
+	Updated_at       time.Time          `json:"updated_at"`
 }
