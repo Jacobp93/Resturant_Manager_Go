1f068d6474074386e2287317256a3e38e375b541
Jacob <jacobp1997@live.co.uk>
2022-06-14 22:05:37 +0100

    main

--- Resturant_Backend_Management/controllers/menuController.go
+++ Resturant_Backend_Management/controllers/menuController.go
@@ -2,9 +2,21 @@ package controller
 
 import "github.com/gin-gonic/gin"
 
+var menuCollection *mongo.Collection = database.OpenCollection(database.Client , "menu")
+
 func GetMenus() gin.HandlerFunc {
 	return func(c *gin.Context) {
-
+		var ctx, cancel = Context.WithTimeout(Context.Background(), 100*time.Second)
+		result, err := menuCollection.Find(Context.TODO(), bson.M{})
+		defer cancel()
+		if err != nil {
+			c.JSON(http.StatusInternalServerError, gin.H{"error": "error occured while listing the menu itself"})
+		}
+		var allMenus []bson.m
+		if err = result.ALL(ctx, &allMenus); err != nil{
+			log.Fatal(err)
+		}
+		c.JSON(http.StatusOK, allMenus)
 	}
 }
 
